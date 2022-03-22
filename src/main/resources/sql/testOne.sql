ee768bc54c7ec0c71969c58f8f97f574e499f155
lijiaxuan <ljx.weekeight@gmail.com>
2022-03-22 19:10:15 +0800

    sql修改

--- src/main/resources/sql/testOne.sql
+++ src/main/resources/sql/testOne.sql
@@ -4,11 +4,11 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS `testOne`;
 CREATE TABLE `testOne`  (
-  `name` varchar(55) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `address` int(11) NULL DEFAULT NULL,
-  `order_count` bigint(20) NULL DEFAULT NULL,
-  `driver_number` bigint(20) NULL DEFAULT NULL,
-  `vehicle_number` bigint(20) NULL DEFAULT NULL
+  `name` varchar(55) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地区名称',
+  `address` int(11) NULL DEFAULT NULL COMMENT '行政区划',
+  `order_count` bigint(20) NULL DEFAULT NULL COMMENT '订单数',
+  `driver_number` bigint(20) NULL DEFAULT NULL COMMENT '人数',
+  `vehicle_number` bigint(20) NULL DEFAULT NULL COMMENT '车辆数'
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
--- src/main/resources/sql/testTwo.sql
+++ src/main/resources/sql/testTwo.sql
@@ -4,9 +4,9 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS `testTwo`;
 CREATE TABLE `testTwo`  (
-  `longitude` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `dimensionality` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `division` int(11) NULL DEFAULT NULL,
+  `longitude` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '经度',
+  `dimensionality` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '维度',
+  `division` int(11) NULL DEFAULT NULL COMMENT '行政区划',
   `identifier` varchar(18) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '预留，地图坐标展示信息唯一编号'
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
